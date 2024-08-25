4dca4b87d61058d9ff0f1f29cb9eb400e7c064b2
Mihir Sastry <94382275+mihiris-here@users.noreply.github.com>
2024-08-25 19:22:25 -0400

    Go to Exercise 3

--- tests/factories.py
+++ tests/factories.py
@@ -17,6 +17,8 @@
 """
 Test Factory to make fake objects for testing
 """
+
+# Extra code
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
